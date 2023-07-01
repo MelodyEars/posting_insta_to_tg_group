9842f9d18ce24e19af503a5aa89d9850f1124a1a
albedo <danya221299@gmail.com>
2023-07-01 23:23:16 +0300

    fix query user tg to db

--- database/query/users.py
+++ database/query/users.py
@@ -7,7 +7,7 @@ def get_user_by_tg_id(chat_id_user: int) -> TelegramUser:
     return user
 
 
-def db_get_access_id_users() -> list[int]:
+def db_get_access_id_users(tg_chat_id: int) -> TelegramUser:
     with db:
-        users = TelegramUser.select().where(TelegramUser.status_user == 'regular_user')  # add date end paid > now
-        return [user.chat_id_user for user in users]
+        users = TelegramUser.get(chat_id_user=tg_chat_id)  # check date end paid > now and status user
+        return users
