3b8df10650d716b81cb9d82f7d101471b2d3b29d
albedo <danya221299@gmail.com>
2023-07-02 12:35:08 +0300

    set up settings

--- SETTINGS.py
+++ SETTINGS.py
@@ -1,7 +1,7 @@
 
 import peewee as pw
 
-
+docker = False
 admins_id = [487950394]
 
 TOKEN = '5839603708:AAGAr8PgN6u8WjpbxWEC2Ni3uhngsgW8i7A'
@@ -9,23 +9,29 @@ TOKEN = '5839603708:AAGAr8PgN6u8WjpbxWEC2Ni3uhngsgW8i7A'
 executable_path = '/usr/bin/chromium'
 # executable_path = None
 
-set_database = {
-                    "user": 'postgres',
-                    "password": 'root123',
-                    "host": "localhost",
-                    "port": 5432,
-                    }
 
+if docker:
+    set_database = {
+                        "user": 'postgres',
+                        "password": 'admin',
+                        "host": "db",
+                        "port": 5432,
+                        }
 
-# set_database = {
-#                     "user": 'postgres',
-#                     "password": 'admin',
-#                     "host": "db",
-#                     "port": 5432,
-#                     }
+else:
+    set_database = {
+                        "user": 'postgres',
+                        "password": 'root123',
+                        "host": "localhost",
+                        "port": 5432,
+                        }
+
 
 db = pw.PostgresqlDatabase('admin', **set_database)
 
 
 # Browsers settings
-TIKTOK_BROWSER_HEADLESS = True
+if docker:
+    TIKTOK_BROWSER_HEADLESS = True
+else:
+    TIKTOK_BROWSER_HEADLESS = False
