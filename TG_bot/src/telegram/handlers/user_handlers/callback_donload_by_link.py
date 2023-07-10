1c4d867907ceae00beb33050186c9317a590e545
albedo <danya221299@gmail.com>
2023-07-10 12:47:54 +0300

    fsm

--- TG_bot/src/telegram/handlers/user_handlers/callback_donload_by_link.py
+++ TG_bot/src/telegram/handlers/user_handlers/callback_donload_by_link.py
@@ -5,6 +5,7 @@ from aiogram.fsm.context import FSMContext
 from loguru import logger
 
 from TG_bot.src.telegram.buttons.user_btn import one_btn
+from TG_bot.src.telegram.handlers.fsm_h.download_by_link import TikTokOneVideo
 from TG_bot.src.telegram.handlers.user_handlers.callback_autoposing import user_router
 from TG_bot.src.telegram.messages.user_msg import DownloadByLinkMessages, MESSAGES
 
@@ -12,7 +13,7 @@ from TG_bot.src.telegram.messages.user_msg import DownloadByLinkMessages, MESSAG
 @user_router.callback_query(Text("download_by_link_tt"))
 async def download_tt_video_by_link(callback: types.CallbackQuery, state: FSMContext):
     message = callback.message
-    logger.info(f"download by link tiktok video {message.from_user.username}")
+    logger.info(f"to have callback")
 
-    await state.set_state()
+    await state.set_state(TikTokOneVideo.link)
     await message.reply(DownloadByLinkMessages['enter_link'], reply_markup=one_btn(MESSAGES['back']))
